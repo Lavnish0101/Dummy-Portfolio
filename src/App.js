88e89084de6ba0d94a5672f7df11f83b12d9403b
Lavnish Kumar <112757643+Lavnish0101@users.noreply.github.com>
2024-03-26 17:30:17 +0530

    Code Upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import Navbar from "./scenes/Navbar";
+import { useEffect, useState } from "react";
+import Dotgroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
+import Projects from "./scenes/Projects";
+import Testimonials from "./scenes/Testimonials";
+import MySkills from "./scenes/MySkills";
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
+import LineGradient from "./components/LineGradient";
+import useMediaQuery from "./hooks/useMediaQuery";
+function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreen && (
+          <Dotgroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <MySkills />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <Projects />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <Testimonials />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <Contact />
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
